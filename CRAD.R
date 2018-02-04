c54aa2411b286fbd4b840fdfdf616b01a8a712f3
vlyubchich <lyubchic@umces.edu>
2018-02-04 15:37:18 -0500

    Update with i.tails and q.tails only, without CRAD, etc.

--- CRAD.R
+++ CRAD.R
@@ -3,14 +3,14 @@ crad <- function(X, Nbin, StepSize = 1, Rcov = FALSE){
   #require(rPython)
   #require(robustbase)
 
-  if(Rcov != FALSE){
+  if(Rcov){
     cov = robustbase::covMcd(X)$cov
   }else{
     cov = cov(X)
   }
 
-  rPython::python.load( "CRAD.py" )
-  res_cl = rPython::python.call( "CRAD",X, StepSize, Nbin, cov)
+  rPython::python.load("CRAD.py")
+  res_cl = rPython::python.call("CRAD", X, StepSize, Nbin, cov)
   return(res_cl)
 }
 
