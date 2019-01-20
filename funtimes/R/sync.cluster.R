5a3fd7a6d457b24a67aa278afcc93f2d77a1d42b
Slava <lyubchich@umces.edu>
2019-01-20 00:07:14 -0500

    Renamed the function and Rd file; need to complete the help file

--- funtimes/R/sync.cluster.R
+++ funtimes/R/sync.cluster.R
@@ -1,11 +1,7 @@
-Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
-    require(funtimes) 
-    # The matrix should have each column with equal number of rows. 
-    ######################### Clustering #######################################
-
+sync.cluster <- function(formula, rate = 1, alpha = 0.05, ...){
     # Storing the final list of clusters 
     Lfinal <- list() 
-    clus_col.Idx <- list() # Sroring the index of columns in cluster
+    clus_col.Idx <- list() # Storing the index of columns in cluster
     
     # Storing values for each category
     sync.pval.Lst = list()
@@ -22,13 +18,11 @@ Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
     DNAME <- splt[1]
     sh <- splt[2]
     
-    
     Y <- as.data.frame(eval(parse(text = DNAME))) # Reading data 
     
     # initializing variables according to the algorithm
     Y_star <<- Y
     
-
     # number of columns in a matrix
     N <- ncol(Y_star)
     # number of rows in a matrix
@@ -41,22 +35,16 @@ Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
     # assigning column names
     colnames(Y_star) <- 1:N
     colnames(Y) <- 1:N
-
     
     while(!is.null(ncol(Y))){
-        
         if (ncol(Y_star) == 0 || is.null(ncol(Y_star))){break}
-        
-        # synchornism test on Y star
+        # synchornism test on Ystar
         SyncResults <- do.call(sync.test,args=list(as.formula(paste("Y_star","~",sh)), ...))
-
-        
         # if we fail to reject the Null Hypothesis
         if(SyncResults$p.value>= alpha)
         {
             # finding common series
             j = intersect(colnames(Y),colnames(Y_star))
-            
             # assigning the cluster number to cluster label variable
             j1 = as.numeric(j)
             L[j1] = K
@@ -65,8 +53,6 @@ Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
             # updating Y star
             Y_star <- Y
             N <- ncol(Y_star)
-            
-            
             # storing the clusters and their results
             sync.stat.Est.Lst[[K]] = SyncResults$estimate$common_trend_estimates
             sync.pval.Lst[[K]] = SyncResults$p.value
@@ -75,9 +61,7 @@ Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
             sync.window_used.Lst[[K]] = SyncResults$estimate$Window_used
             sync.all_consideredWindow.Lst[[K]] = SyncResults$estimate$all_considered_windows
             sync.wavk_obs.Lst[[K]] = SyncResults$estimate$wavk_obs
-            
             K = K+1
-            
         } else {
             # extracting local factor statistics
             WAVKResults <- abs(SyncResults$estimate$wavk_obs)
@@ -88,11 +72,8 @@ Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
             WAVKtmp.rmv <- WAVKtmp[(length(WAVKtmp)-nRM+1):length(WAVKtmp)]
             # removing the time series as per the rate
             Y_star <- Y_star[, !(WAVKResults == WAVKtmp.rmv)]
-            
         }
-        
-        # lines 21 to 29
-        if(is.vector(Y_star)){
+        if (is.vector(Y_star)) {
             # finding the position of the matching series
             for ( idx.j in 1:length(colnames(Y))){if (length(which(Y[,idx.j] == Y_star) == TRUE) == nrows){j = idx.j}}
             # Extracting the correct column name from the original Y
@@ -105,23 +86,16 @@ Clustering <- function(formula, rate = 1, alpha = 0.05, ...){
             Y <- Y[ , -j]
             Y_star <- Y
             N <- ncol(Y_star)
-            
         }
-        
-        
     }
     Lfinal <- L
-    
     clus_col.Idx <- sapply(1:max(unique(L[!is.nan(L)])), function(x) which(L == x)) 
     clus_col.NoBind <- which(L == 0)
-    
     print(list(Clusters=table(Lfinal)))
     invisible(structure(list(Clusters = Lfinal,Column.Index=clus_col.Idx,Pval = sync.pval.Lst,
-                   TestStatistics = sync.Teststat.Lst,Estimate = sync.stat.Est.Lst, AROrder = sync.ar_order.Lst,
-                   WindowUsed = sync.window_used.Lst,
-                   allConsideredWindow = sync.all_consideredWindow.Lst, WAVKobs = sync.wavk_obs.Lst)))
-    
+                             TestStatistics = sync.Teststat.Lst,Estimate = sync.stat.Est.Lst, AROrder = sync.ar_order.Lst,
+                             WindowUsed = sync.window_used.Lst,
+                             allConsideredWindow = sync.all_consideredWindow.Lst, WAVKobs = sync.wavk_obs.Lst)))
     # removing Y_star 
     rm(Y_star, envir = parent.frame())
-    
 }
