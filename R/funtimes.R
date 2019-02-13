b4a55405999a530b7b727dbba5e693f207ab9ea1
Slava <lyubchich@umces.edu>
2019-02-13 17:26:18 -0500

    added Beale's estimator function

--- R/funtimes.R
+++ R/funtimes.R
@@ -2,6 +2,6 @@
 #' @importFrom Jmisc demean
 #' @importFrom Kendall MannKendall
 #' @importFrom Rdpack reprompt
-#' @importFrom stats ar arima.sim as.formula dist embed filter IQR lm median na.omit pnorm quantile rnorm sd var
+#' @importFrom stats ar arima.sim as.formula cov dist embed filter IQR lm median na.omit pnorm qnorm quantile rnorm sd var
 #' @importFrom utils combn
 NULL
