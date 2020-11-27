0bd33d2840b584b6140855e9d6a7c1b87651483b
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2020-11-27 12:50:56 -0500

    speedup purity

--- R/Purity.R
+++ R/Purity.R
@@ -42,6 +42,7 @@
 #' @export
 #' @examples
 #' # Fix seed for reproducible simulations:
+#' # RNGkind(sample.kind = "Rounding") #run this line to have same seed across R versions > R 3.6.0
 #' set.seed(1)
 #' 
 #' ##### Example 1
@@ -84,21 +85,16 @@
 #' 
 purity <- function(classes, clusters) 
 {
-  tmp <- cbind(classes, clusters)
-  tmp <- tmp[order(tmp[,1]), ]
-  classes <- tmp[,1]
-  clusters <- tmp[,2]
-  nClasses <- length(unique(classes))
-  nClusters <- length(unique(clusters))
   ClassLabels <- ClusterLabels <- ClusterSize <- numeric()
   tmp <- table(classes, clusters)
-  for (i in 1:min(dim(tmp))) {
+  i <- 1
+  while (min(dim(tmp)) > 0) {
     ind <- which(tmp == max(tmp), arr.ind = TRUE)
     if (nrow(ind) > 1) { #several equal max values
       if (any(dim(tmp) < 2)) {
         ind <- ind[1,]
       } else {
-        #for each class with equal max, run and select the second max and choose minimum of that
+        #for each class with equal max, select the second max and choose minimum of that
         ind <- ind[which.min(sapply(1:nrow(ind), function(j) 
           sort(tmp[,ind[j,2]], decreasing = TRUE)[2])), ]
       }
@@ -106,18 +102,12 @@ purity <- function(classes, clusters)
     ClassLabels[i] <- rownames(tmp)[ind[1]]
     ClusterLabels[i] <- colnames(tmp)[ind[2]]
     ClusterSize[i] <- max(tmp)
-    tmp <- tmp[-which(rownames(tmp) == ClassLabels[i]), -which(colnames(tmp) == ClusterLabels[i])]
-    #If converged to only one column (cluster), tmp should be a column-vector:
-    if (is.null(dim(tmp))) {
-      if (nClusters < nClasses) { 
-        tmp <- as.matrix(tmp)
-        colnames(tmp) <- unique(clusters)[!is.element(unique(clusters), ClusterLabels)]
-      } else {#tmp should be a row-vector
-        tmp <- t(as.matrix(tmp))
-        rownames(tmp) <- unique(classes)[!is.element(unique(classes), ClassLabels)]
-        if (length(tmp) == 1) colnames(tmp) <- unique(clusters)[!is.element(unique(clusters), ClusterLabels)]
-      }
-    }
+    tmp <- tmp[-which(rownames(tmp) == ClassLabels[i]), 
+               -which(colnames(tmp) == ClusterLabels[i]), drop = FALSE]
+    # Reevaluate the remaining table to remove all-zero rows or columns, if any
+    tmp <- tmp[rowSums(tmp) != 0, , drop = FALSE]
+    tmp <- tmp[, colSums(tmp) != 0, drop = FALSE]
+    i <- i + 1
   }
   out <- data.frame(ClassLabels, ClusterLabels, ClusterSize)
   out <- out[order(out$ClassLabels),]
